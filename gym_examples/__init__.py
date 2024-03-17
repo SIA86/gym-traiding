d490fec5f969be0802b29f913841f688ce4aee12
Igor Saf <sfron4ik@gmail.com>
2024-03-17 21:10:25 +0300

    namespace changed

--- gym_examples/__init__.py
+++ gym_examples/__init__.py
@@ -1,7 +1,7 @@
 from gymnasium.envs.registration import register
 
 register(
-     id="gym_examples/MonoCryptoEnv-v0",
+     id="MonoCryptoEnv-v0",
      entry_point="gym_examples.envs:TradingEnv",
      max_episode_steps=1e6,
 )
